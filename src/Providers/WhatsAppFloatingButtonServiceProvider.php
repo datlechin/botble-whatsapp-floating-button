88e59e4e30a903c59937a3054de74b32daee02eb
Sang Nguyen <sangnguyenplus@gmail.com>
2024-03-02 13:16:24 +0700

    Reformat code

--- src/Providers/WhatsAppFloatingButtonServiceProvider.php
+++ src/Providers/WhatsAppFloatingButtonServiceProvider.php
@@ -29,21 +29,32 @@ class WhatsAppFloatingButtonServiceProvider extends ServiceProvider
                         'id' => 'plugins-whatsapp-floating-button',
                         'priority' => 9999,
                         'name' => 'plugins/whatsapp-floating-button::whatsapp-floating-button.name',
-                        'icon' => version_compare('7.0.0', get_core_version(), '<=') ? 'ti ti-brand-whatsapp' : 'fab fa-whatsapp',
+                        'icon' => version_compare(
+                            '7.0.0',
+                            get_core_version(),
+                            '<='
+                        ) ? 'ti ti-brand-whatsapp' : 'fab fa-whatsapp',
                         'route' => 'whatsapp-floating-button.settings',
                     ]);
 
                 if (setting('whatsapp-floating-button.enabled')) {
                     Theme::asset()
                         ->usePath(false)
-                        ->add('floating-wpp-css', asset('vendor/core/plugins/whatsapp-floating-button/css/floating-wpp.min.css'));
+                        ->add(
+                            'floating-wpp-css',
+                            asset('vendor/core/plugins/whatsapp-floating-button/css/floating-wpp.min.css')
+                        );
 
                     Theme::asset()
                         ->container('footer')
                         ->usePath(false)
-                        ->add('floating-wpp-js', asset('vendor/core/plugins/whatsapp-floating-button/js/floating-wpp.min.js'), ['jquery']);
+                        ->add(
+                            'floating-wpp-js',
+                            asset('vendor/core/plugins/whatsapp-floating-button/js/floating-wpp.min.js'),
+                            ['jquery']
+                        );
 
-                    add_filter(THEME_FRONT_FOOTER, function (string |null $data): string {
+                    add_filter(THEME_FRONT_FOOTER, function (string|null $data): string {
                         return $data . view('plugins/whatsapp-floating-button::show');
                     });
                 }
